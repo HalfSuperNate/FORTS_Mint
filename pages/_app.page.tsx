2d518ca2afa1dcee3d45744f9926a9764b744cda
HalfSuperNate <n8kulis@gmail.com>
2024-02-14 01:28:35 -0600

    first push

---
+++ pages/_app.page.tsx
@@ -0,0 +1,46 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import type { AppProps } from 'next/app';
+import { WagmiProvider } from 'wagmi';
+import {
+  // arbitrum,
+  // goerli,
+  // mainnet,
+  // optimism,
+  // polygon,
+  // zora,
+  polygonMumbai
+} from 'wagmi/chains';
+import { QueryClientProvider, QueryClient, } from '@tanstack/react-query';
+
+const config = getDefaultConfig({
+  appName: 'FORTS',
+  projectId: '43e1f16f66aacf153df114054a18202b',
+  chains: [
+    //mainnet, 
+    //polygon, 
+    //optimism, 
+    //arbitrum, 
+    //base, 
+    //zora,
+    polygonMumbai
+  ],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider modalSize="compact" theme={darkTheme()}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.page.tsx
@@ -0,0 +1,65 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import React from 'react';
+import Wallet from './wallet.page';
+import Link from 'next/link';
+import Image from 'next/image';
+// import AdminComponent from './adminCtrl.page';
+// import { useAccount } from 'wagmi';
+// import { useIsMounted } from './useIsMounted';
+// import { AdminCheck } from './readContract';
+
+const iStyle_a = {
+  left: '-35px'
+}
+
+const iStyle_b = {
+  left: '35px'
+}
+
+const Home: NextPage = () => {
+  // const { address } = useAccount();
+  // const isAdmin = AdminCheck(address);
+  // const mounted = useIsMounted();
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Fortunates Mint</title>
+        <meta name="description" content="Fortunates are a mix of randomly picked, generated, and fully on-chain subjects." />
+        <link href="/icon.png" rel="icon" type="image/x-icon"/>
+      </Head>
+
+      <main className={styles.main}>
+
+        <Wallet />
+        {/* {mounted ? isAdmin && <AdminComponent /> : null} */}
+        
+      </main>
+
+      <div className={styles.bottomImg}>
+        <Image fill src="/bottomImg.png" alt="Logo" />
+      </div>
+      
+      <footer className={styles.footer}>
+        <Link href="https://testnets.opensea.io/assets/mumbai/0x401fd2d3f0d48ac2e9e9e4ae63589c801b855eff" rel="noopener noreferrer" target="_blank">
+          <div className={styles.footerLogo}>
+            <Image width={30} height={30} src="/opensea_icon.svg" alt="OpenSea"/>
+          </div>
+        </Link>
+        <Link href="https://mumbai.polygonscan.com/address/0x401fd2d3f0d48ac2e9e9e4ae63589c801b855eff" rel="noopener noreferrer" target="_blank">
+          <div className={styles.footerLogo}>
+            <Image width={30} height={30} src="/polygonscan.svg" alt="Etherscan"/>
+          </div>
+        </Link>
+        <Link href="https://twitter.com/EasyNFT_" rel="noopener noreferrer" target="_blank">
+          <div className={styles.footerLogo}>
+            <Image width={30} height={30} src="/x_icon.svg" alt="Twitter"/>
+          </div>
+        </Link>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
